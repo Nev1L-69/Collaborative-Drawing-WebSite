bf12625b69e608b24b8680d4bce77df4c12989db
V0LT0R <119123327+V0LT0R@users.noreply.github.com>
2024-11-07 16:38:01 +0500

    generate_id

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,32 @@ import Forms from "./components/Forms";
 import RoomPage from "./pages/RoomPage";
 
 const App = () => {
+
+  const uuid = () => {
+    var S4 = () => {
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+
+    };
+    return (
+      S4() +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      S4() +
+      S4()
+    )
+  }
   return (
       <div className='container'>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Forms />} />
+            <Route path="/" element={<Forms uuid={uuid}/>} />
             <Route path="/:roomId" element={<RoomPage />} />
 
           </Routes>
